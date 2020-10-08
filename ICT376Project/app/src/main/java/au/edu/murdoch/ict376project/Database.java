a3e14d06475bdda6a4e19c7aca8b78c98a40ba85
EdwardPool <eddiepool@healthystridesfoundation.com>
2020-10-08 17:07:08 +0800

    Update of fragments, layouts and nav menu

--- ICT376Project/app/src/main/java/au/edu/murdoch/ict376project/Database.java
+++ ICT376Project/app/src/main/java/au/edu/murdoch/ict376project/Database.java
@@ -1,5 +1,6 @@
 package au.edu.murdoch.ict376project;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -7,6 +8,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class Database extends SQLiteOpenHelper
 {
     public static final String DB_NAME = "database.db";
+    private SQLiteDatabase myDb;
 
     public static final String CUSTOMER_TABLE = "CUSTOMER";
     public static final String CUSTOMER_ID = "id";
@@ -58,6 +60,29 @@ public class Database extends SQLiteOpenHelper
     public void onUpgrade(SQLiteDatabase db, int i, int i1)
     {
 
+    }
+
+    public long createItem(int pId, String pName, int pPrice, String pDescription, String pRating, String pPlatform, int pStock) {
+
+        ContentValues initialValues = new ContentValues();
+        initialValues.put(PRODUCT_ID, pId);
+        initialValues.put(PRODUCT_NAME, pName);
+        initialValues.put(PRODUCT_PRICE, pPrice);
+        initialValues.put(PRODUCT_DESCRIPTION, pDescription);
+        initialValues.put(PRODUCT_RATING, pRating);
+        initialValues.put(PRODUCT_PLATFORM, pPlatform);
+        initialValues.put(PRODUCT_STOCK, pStock);
+
+        return myDb.insert(PRODUCT_TABLE, null, initialValues);
+    }
+
+    public void insertMyShopItems() {
+        createItem(1,"Mario Odyssey", 69, "3D adventures of Mario", "All ages", "Nintendo", 5);
+        createItem(2,"Mario Kart", 69, "Kart racing with Mario", "All ages", "Nintendo", 5);
+        createItem(3,"Zelda", 69, "3D adventures of Link", "8 years+", "Nintendo", 5);
+        createItem(4,"Witcher3", 69, "Third episode of the Witcher", "15 years+", "Nintendo", 5);
+        createItem(5,"FIFA", 69, "Football by FIFA", "All ages", "Nintendo", 5);
+        // etc etc
     }
 
 
