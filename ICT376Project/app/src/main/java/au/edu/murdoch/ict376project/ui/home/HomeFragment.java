b8b718b0c94ee0058a83e74297b375fbea506c11
EdwardPool <eddiepool@healthystridesfoundation.com>
2020-10-10 18:15:47 +0800

    added comments for accessing images - I will use the same images in the listview for the games/cart.

--- ICT376Project/app/src/main/java/au/edu/murdoch/ict376project/ui/home/HomeFragment.java
+++ ICT376Project/app/src/main/java/au/edu/murdoch/ict376project/ui/home/HomeFragment.java
@@ -27,16 +27,30 @@ public class HomeFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
         Random rand = new Random();
+        // get db
         Database db = new Database(getActivity());
 
         for(int i = 1; i < 6; i++)
         {
+            // picture = view.findViewById(int); int value = getResources().getIdentifier(
             ImageView view = root.findViewById(getResources().getIdentifier("deal" + i, "id", getActivity().getPackageName()));
 
             int id = rand.nextInt((20 - 1) + 1) + 1;
+            // get cursor returned from db
             Cursor res = db.getProductById(id);
 
-            view.setImageResource(getResources().getIdentifier(res.getString(res.getColumnIndex("file")), "drawable", getActivity().getPackageName()));
+            // name of the entry in column no. (the one called "file") - as a String
+            String mName = res.getString(res.getColumnIndex("file"));
+            // type is drawable
+            String mDefType = "drawable";
+            // package = getActivity().getPackageName();
+            String mDefPackage = getActivity().getPackageName();
+            // resource id = getResources().getIdentifier(String name, String defType, String defPackage);
+            int resId = getResources().getIdentifier(mName, mDefType, mDefPackage);
+            // set the view to the relevant id
+            view.setImageResource(resId);
+
+            //view.setImageResource(getResources().getIdentifier(res.getString(res.getColumnIndex("file")), "drawable", getActivity().getPackageName()));
         }
 
         return root;
