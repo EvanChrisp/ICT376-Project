200a5cc2cf770724254acfcdd24cb197103467aa
EvanIMK <epchrisp@bigpond.com>
2020-10-11 20:47:31 +0800

    Make more modular

--- ICT376Project/app/src/main/java/au/edu/murdoch/ict376project/ui/home/HomeFragment.java
+++ ICT376Project/app/src/main/java/au/edu/murdoch/ict376project/ui/home/HomeFragment.java
@@ -3,7 +3,6 @@ package au.edu.murdoch.ict376project.ui.home;
 import android.database.Cursor;
 import android.graphics.Paint;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,26 +11,21 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProviders;
 
 import java.util.Random;
 
 import au.edu.murdoch.ict376project.Database;
 import au.edu.murdoch.ict376project.R;
 
-public class HomeFragment extends Fragment {
-
-    private HomeViewModel homeViewModel;
-
+public class HomeFragment extends Fragment
+{
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         int i = 1;
         int[] ids = new int[5];
         Random rand = new Random();
-        // get db
-        Database db = new Database(getActivity());
+        Database db = new Database(getActivity()); //get db
 
         // loop through 5 deals
         while(i <= 5)
@@ -46,32 +40,18 @@ public class HomeFragment extends Fragment {
             // get cursor returned from db
             Cursor res = db.getProductById(id);
 
-            // widget = view.findViewById(getResources().getIdentifier(String name, String type, String package);
             // String name is the id in the XML - type is "id" for i.d. , package = getActivity().getPackageName()
             ImageView image = root.findViewById(getResources().getIdentifier("deal" + i, "id", getActivity().getPackageName()));
+            setDealImage(image, res);
+
             TextView name = root.findViewById(getResources().getIdentifier("dealtext" + i, "id", getActivity().getPackageName()));
+            setDealName(name, res);
+
             TextView priceOld = root.findViewById(getResources().getIdentifier("dealoldprice" + i, "id", getActivity().getPackageName()));
+            setDealOldPrice(priceOld, res);
+
             TextView priceNew = root.findViewById(getResources().getIdentifier("dealnewprice" + i, "id", getActivity().getPackageName()));
-
-            // name of the entry in column no. (the one called "file") - as a String
-            String mFile = res.getString(res.getColumnIndex("file"));
-            String mName = res.getString(res.getColumnIndex("name"));
-            String mPrice = res.getString(res.getColumnIndex("price"));
-
-            // type is drawable
-            String mDefType = "drawable";
-            // package = getActivity().getPackageName();
-            String mDefPackage = getActivity().getPackageName();
-            // resource id = getResources().getIdentifier(String name, String defType, String defPackage);
-            int resId = getResources().getIdentifier(mFile, mDefType, mDefPackage);
-            // set the view to the relevant id
-
-            image.setImageResource(resId);
-            name.setText(mName);
-            priceOld.setText("$" + mPrice);
-            priceOld.setPaintFlags(priceOld.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
-            double price = Integer.parseInt(mPrice) * 0.8;
-            priceNew.setText("$" + (int)price);
+            setDealNewPrice(priceNew, res);
 
             i++;
         }
@@ -88,5 +68,38 @@ public class HomeFragment extends Fragment {
                 return true;
         }
         return false;
+    }
+
+    private void setDealImage(ImageView view, Cursor res)
+    {
+        // name of the entry in column no. (the one called "file") - as a String
+        String mFile = res.getString(res.getColumnIndex("file"));
+        String mDefType = "drawable";
+        String mDefPackage = getActivity().getPackageName();
+
+        // resource id = getResources().getIdentifier(String name, String defType, String defPackage);
+        int resId = getResources().getIdentifier(mFile, mDefType, mDefPackage);
+        // set the view to the relevant id
+        view.setImageResource(resId);
+    }
+
+    private void setDealName(TextView view, Cursor res)
+    {
+        String mName = res.getString(res.getColumnIndex("name"));
+        view.setText(mName);
+    }
+
+    private void setDealOldPrice(TextView view, Cursor res)
+    {
+        String mPrice = res.getString(res.getColumnIndex("price"));
+        view.setText("$" + mPrice);
+        view.setPaintFlags(view.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+    }
+
+    private void setDealNewPrice(TextView view, Cursor res)
+    {
+        String mPrice = res.getString(res.getColumnIndex("price"));
+        double price = Integer.parseInt(mPrice) * 0.8;
+        view.setText("$" + (int)price);
     }
 }
--- ICT376Project/app/src/main/java/au/edu/murdoch/ict376project/ui/home/HomeViewModel.java
+++ ICT376Project/app/src/main/java/au/edu/murdoch/ict376project/ui/home/HomeViewModel.java
@@ -1,7 +1,5 @@
 package au.edu.murdoch.ict376project.ui.home;
 
-import android.widget.ImageView;
-
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
