53706ef0fa90054ada25ced511e0c12ba3074b77
Razecar <55474694+Razecar@users.noreply.github.com>
2020-10-15 13:14:24 +0800

    Update Database.java

--- ICT376Project/app/src/main/java/au/edu/murdoch/ict376project/Database.java
+++ ICT376Project/app/src/main/java/au/edu/murdoch/ict376project/Database.java
@@ -157,24 +157,44 @@ public class Database extends SQLiteOpenHelper
         addProduct(3, "Zelda", 69, "zelda", "3D adventures of Link", "8 years+", "Nintendo", "0");
         addProduct(4, "The Witcher 3", 69, "witcher3_nintendo", "Third episode of the Witcher", "15 years+", "Nintendo", "0");
         addProduct(5, "FIFA 19", 69, "fifa_nintendo", "Football by FIFA", "All ages", "Nintendo", "0");
+        addProduct(6, "Pokemon Shield", 69, "pokemon_shield", "Pokemon Shield Edition", "All ages", "Nintendo", "0");
+        addProduct(7, "Pokemon Sword", 69, "pokemon_sword". "Pokemom Sword Edition", "All ages", "Nintendo", "0");
+        addProduct(8, "Pokemon Mystery Dungeon", 69, "pokemon_mystery_dungeon", "Pokemon Mystery Dungeon Remake", "All ages", "Nintendo", "0");
+        addProduct(9, "Mario Party", 69, "mario_party", "Latest Mario Party" "All ages", "Nintendo", "0");
+        addproduct(10, "Overcooked 2", 69, "overcooked_2", "Cook with friends", "All ages", "Nintendo", "0");
 
-        addProduct(6, "Halo", 89, "halo", "Adventures of MasterChief","15 years+", "Xbox", "0");
-        addProduct(7, "FIFA 19", 79, "fifa_xbox", "Football by FIFA","All ages", "Xbox", "0");
-        addProduct(8, "Final Fantasy XXV", 99, "ffxv_xbox", "JRPG masterpiece","15 years+", "Xbox", "0");
-        addProduct(9, "Forza", 119, "forza", "Racing Sim","All ages", "Xbox", "0");
-        addProduct(10, "Gears of War", 59, "gears_of_war", "3rd person shooter", "15 years+", "Xbox", "0");
+        addProduct(11, "Halo", 89, "halo", "Adventures of MasterChief","15 years+", "Xbox", "0");
+        addProduct(12, "FIFA 19", 79, "fifa_xbox", "Football by FIFA","All ages", "Xbox", "0");
+        addProduct(13, "Final Fantasy XXV", 99, "ffxv_xbox", "JRPG masterpiece","15 years+", "Xbox", "0");
+        addProduct(14, "Forza", 119, "forza", "Racing Sim","All ages", "Xbox", "0");
+        addProduct(15, "Gears of War", 59, "gears_of_war", "3rd person shooter", "15 years+", "Xbox", "0");
+        addProduct(16, "Dark Souls 3", 44, "dark_souls_3_xbox", "Final Game of Dark Souls Trilogy", "15 years+", "Xbox", "0");
+        addProduct(17, "Modern Warfare", 99, "modern_warfare_xbox", "Latest Call of Duty Game", "15 years+", "Xbox", "0");
+        addProduct(18, "Metro Exodus", 30, "metro_exodus_xbox", "Explore a Frozen Apocolyptic Russia", "15 years+", "Xbox", "0");
+        addProduct(19, "Sunset Overdrive", 10, "sunset_overdrive", "Xbox Launch Title", "15 years+", "Xbox", "0");
+        addProduct(20, "Jedi Fallen Order", 48, "jedi_fallen_order_xbox", "Play as a Jedi and fight the Empire", "15 years+", "Xbox", "0");
 
-        addProduct(11, "Crash Bandicoot", 49, "crash_bandicoot", "Adventures of Crash", "All ages", "Playstation", "0");
-        addProduct(12, "Final Fantasy XV", 99, "ffxv_ps", "JRPG masterpiece", "15 years+", "Playstation", "0");
-        addProduct(13, "Ridge Racer", 99, "ridge_racer", "Arcade Racer", "All ages", "Playstation", "0");
-        addProduct(14, "Spiderman", 89, "spiderman", "The adventures of Peter Parker", "All ages", "Playstation", "0");
-        addProduct(15, "Red Dead Redemption 2", 69, "rdr2", "Open-world wild west", "15 years+", "Playstation", "0");
+        addProduct(21, "Crash Bandicoot", 49, "crash_bandicoot", "Adventures of Crash", "All ages", "Playstation", "0");
+        addProduct(22, "Final Fantasy XV", 99, "ffxv_ps", "JRPG masterpiece", "15 years+", "Playstation", "0");
+        addProduct(23, "Ridge Racer", 99, "ridge_racer", "Arcade Racer", "All ages", "Playstation", "0");
+        addProduct(24, "Spiderman", 89, "spiderman", "The adventures of Peter Parker", "All ages", "Playstation", "0");
+        addProduct(25, "Red Dead Redemption 2", 69, "rdr2", "Open-world wild west", "15 years+", "Playstation", "0");
+        addProduct(26, "Dark Souls 3", 44, "dark_souls_3_ps4", "Final Game of Dark Souls Trilogy", "15 years+", "Playstation", "0");
+        addProduct(27, "Modern Warfare", 99, "modern_warfare_ps4", "Latest Call of Duty Game", "15 years+", "Playstation", "0");
+        addProduct(28, "Spyro Reignited Trilogy", 70, "spyro_ps4", "Remake of all three original spyro games", "All ages", "Playstation", "0");
+        addProduct(29, "God of War", 25, "god_of_war_ps4", "Long awaited sequal to the God of War Franchise", "15 years+", "Playstation", "0");
+        addProduct(30, "Bloodborne", 20, "bloodborne", "Spin off of Dark Souls Franchise", "15 years+", "Playstation", "0");
 
-        addProduct(16, "The Witcher 3", 49, "witcher3_pc", "Third episode of the Witcher", "15 years+", "PC", "0");
-        addProduct(17, "Call of Duty", 49, "call_of_duty", "First Person Shooter", "15 years+", "PC", "0");
-        addProduct(18, "MS Flight Simulator", 99, "ms_flight_sim", "Flight simulator", "All ages", "PC", "0");
-        addProduct(19, "Monster Hunter World", 79, "monster_hunter", "The world of monsters", "All ages", "PC", "0");
-        addProduct(20, "Half-Life 3", 149, "half_life", "Third episode of Half Life", "15 years+", "PC", "0");
+        addProduct(31, "Team Fortress 2", 25, "tf2", "Classic FPS", "15 years+", "PC", "0");
+        addProduct(32, "Command and Conquer Red Alert 3", 30, "c_and_c_red_alert_3", "Wacky Strategy Game", "15 years+", "PC", "0");
+        addProduct(33, "Among Us", 5, "among_us", "Social Deceoption game with friends", "15 years+", "PC", "0");
+        addProduct(34, "Dark Souls 3", 44, "dark_souls_3_pc", "Final Game of Dark Souls Trilogy", "15 years+", "PC", "0");
+        addProduct(35, "Rainbow Six Siege", 30, "rainbow_six_siege_PC", "Team Based Shooter", "15 years+", "PC", "0");
+        addProduct(36, "The Witcher 3", 49, "witcher3_pc", "Third episode of the Witcher", "15 years+", "PC", "0");
+        addProduct(37, "Call of Duty", 49, "call_of_duty", "First Person Shooter", "15 years+", "PC", "0");
+        addProduct(38, "MS Flight Simulator", 99, "ms_flight_sim", "Flight simulator", "All ages", "PC", "0");
+        addProduct(39, "Monster Hunter World", 79, "monster_hunter", "The world of monsters", "All ages", "PC", "0");
+        addProduct(40, "Half-Life 3", 149, "half_life", "Third episode of Half Life", "15 years+", "PC", "0");
 
     }
     public int getTotalItemsCount() {
