81eca189874949074705c9fbbf3a3d809fd515b8
EdwardPool <eddiepool@healthystridesfoundation.com>
2020-10-15 16:11:56 +0800

    Login fragment and view model added.

---
+++ ICT376Project/app/src/main/java/au/edu/murdoch/ict376project/ui/login/LoginFragment.java
@@ -0,0 +1,72 @@
+package au.edu.murdoch.ict376project.ui.login;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.util.Pair;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import au.edu.murdoch.ict376project.Database;
+import au.edu.murdoch.ict376project.DetailsActivity;
+import au.edu.murdoch.ict376project.R;
+import au.edu.murdoch.ict376project.ui.nintendo.NintendoViewModel;
+
+public class LoginFragment extends Fragment {
+
+    // model class
+    private LoginViewModel loginViewModel;
+    View mLayoutView;
+
+
+    // Database
+    Database mydb = null;
+    ArrayList mArrayList;  // the list of all products
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // data comes from the nintendoViewModel class
+        loginViewModel = ViewModelProviders.of(this).get(LoginViewModel.class);
+
+        // declare view -> inflate (this fragment, into this container, false);
+        mLayoutView = inflater.inflate(R.layout.fragment_nintendo, container, false);
+
+        // textView = resource
+        final TextView textView = mLayoutView.findViewById(R.id.text_nintendo);
+
+        // use the nintendoViewModel to set the text to String s
+        loginViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+
+        // now return the view
+        return mLayoutView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+
+        super.onActivityCreated(savedInstanceState);
+
+        Toast.makeText(getActivity(), "This is the login fragment", Toast.LENGTH_SHORT).show();
+    }
+
+}
