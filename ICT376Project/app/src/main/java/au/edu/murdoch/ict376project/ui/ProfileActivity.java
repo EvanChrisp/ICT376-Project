453f7bac0c8aa0617d5ac3125e2d9afc810785ec
EdwardPool <eddiepool@healthystridesfoundation.com>
2020-10-19 09:09:48 +0800

    profile page can tell if logged in

--- ICT376Project/app/src/main/java/au/edu/murdoch/ict376project/ui/ProfileActivity.java
+++ ICT376Project/app/src/main/java/au/edu/murdoch/ict376project/ui/ProfileActivity.java
@@ -3,13 +3,26 @@ package au.edu.murdoch.ict376project.ui;
 import androidx.appcompat.app.AppCompatActivity;
 import au.edu.murdoch.ict376project.R;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    TextView username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        username = (TextView)findViewById(R.id.profileTextViewUname);
+
+        SharedPreferences userDetails = getSharedPreferences("prefs", MODE_PRIVATE);
+        String storedUserName = userDetails.getString("username", "");
+
+        username.setText("Username: " +storedUserName);
+
     }
 }
