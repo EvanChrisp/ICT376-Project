da6a8030550983ddc551be8308ec6d73b86d80cb
EdwardPool <eddiepool@healthystridesfoundation.com>
2020-10-22 13:41:37 +0800

    Directions and GPS is now available in activity from the contacts page.

---
+++ ICT376Project/app/src/main/java/au/edu/murdoch/ict376project/DirectionsActivity.java
@@ -0,0 +1,37 @@
+package au.edu.murdoch.ict376project;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class DirectionsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_directions);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng Murdoch = new LatLng(-32.068456, 115.834925);
+        map.addMarker(new MarkerOptions().position(Murdoch).title("ERE Games"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(Murdoch));
+    }
+}
--- ICT376Project/app/src/main/java/au/edu/murdoch/ict376project/ui/contact/ContactFragment.java
+++ ICT376Project/app/src/main/java/au/edu/murdoch/ict376project/ui/contact/ContactFragment.java
@@ -1,19 +1,46 @@
 package au.edu.murdoch.ict376project.ui.contact;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import au.edu.murdoch.ict376project.DirectionsActivity;
 import au.edu.murdoch.ict376project.R;
 
 public class ContactFragment extends Fragment
 {
+    View mLayoutView;
+    Button gMaps;
+
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.fragment_contact, container, false);
+        mLayoutView = inflater.inflate(R.layout.fragment_contact, container, false);
+        gMaps = (Button)mLayoutView.findViewById(R.id.gMaps);
+
+        return mLayoutView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState)
+    {
+        super.onActivityCreated(savedInstanceState);
+
+        gMaps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), DirectionsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void displaySomething() {
+        // todo
     }
 }
