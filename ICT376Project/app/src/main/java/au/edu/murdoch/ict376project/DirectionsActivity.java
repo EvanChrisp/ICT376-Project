27ef9138c3bab3a1adf1c033cb353f09660e1862
EdwardPool <eddiepool@healthystridesfoundation.com>
2020-10-22 18:21:36 +0800

    Google maps fully implemented.

--- ICT376Project/app/src/main/java/au/edu/murdoch/ict376project/DirectionsActivity.java
+++ ICT376Project/app/src/main/java/au/edu/murdoch/ict376project/DirectionsActivity.java
@@ -1,5 +1,6 @@
 package au.edu.murdoch.ict376project;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -17,10 +18,16 @@ import com.google.android.gms.location.LocationServices;
 import android.location.Location;
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
@@ -34,16 +41,37 @@ public class DirectionsActivity extends FragmentActivity implements OnMapReadyCa
     Marker mCurrLocationMarker;
     GoogleApiClient mGoogleApiClient;
     LocationRequest mLocationRequest;
+    String[] locationPermissions;
+    Button requestLocation;
+    final int LOCATION_REQUEST_CODE = 777;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_directions);
+        requestLocation = findViewById(R.id.mapButton);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        requestLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!checkLocationPermissions()){
+                    ActivityCompat.requestPermissions(DirectionsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+                }
+            }
+        });
+
+    }
+
+    private boolean checkLocationPermissions(){
+
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+    }
+
+    private void requestLocationPermissions(){
+        ActivityCompat.requestPermissions(this, locationPermissions, LOCATION_REQUEST_CODE);
     }
 
     @Override
@@ -51,15 +79,33 @@ public class DirectionsActivity extends FragmentActivity implements OnMapReadyCa
         mMap = googleMap;
 
         LatLng Murdoch = new LatLng(-32.068456, 115.834925);
-        mMap.addMarker(new MarkerOptions().position(Murdoch).title("ERE Games"));
+        mMap.addMarker(new MarkerOptions().position(Murdoch).title("ERE Games Murdoch"));
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(Murdoch));
 
+        LatLng Cannington = new LatLng(-32.019438, 115.936375);
+        mMap.addMarker(new MarkerOptions().position(Cannington).title("ERE Games Cannington"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(Cannington));
+
+        LatLng Midland = new LatLng(-31.890344, 116.010405);
+        mMap.addMarker(new MarkerOptions().position(Midland).title("ERE Games Midland"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(Midland));
+
+        LatLng Joondalup = new LatLng(-31.745981, 115.769449);
+        mMap.addMarker(new MarkerOptions().position(Joondalup).title("ERE Games Midland"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(Joondalup));
+
+        LatLng Perth = new LatLng(-31.953096, 115.860720);
+        mMap.addMarker(new MarkerOptions().position(Perth).title("ERE Games Midland"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(Perth));
+
+
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ContextCompat.checkSelfPermission(this,
-                    Manifest.permission.ACCESS_FINE_LOCATION)
-                    == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                 buildGoogleApiClient();
                 mMap.setMyLocationEnabled(true);
+            } else {
+                //Toast.makeText(this, "permission is not given for location", Toast.LENGTH_SHORT).show();
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
             }
         }
         else {
@@ -83,9 +129,7 @@ public class DirectionsActivity extends FragmentActivity implements OnMapReadyCa
         mLocationRequest.setInterval(1000);
         mLocationRequest.setFastestInterval(1000);
         mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
         }
 
@@ -107,7 +151,7 @@ public class DirectionsActivity extends FragmentActivity implements OnMapReadyCa
         LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(latLng);
-        markerOptions.title("Current Position");
+        markerOptions.title("Current Location");
         markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
         mCurrLocationMarker = mMap.addMarker(markerOptions);
 
@@ -125,6 +169,33 @@ public class DirectionsActivity extends FragmentActivity implements OnMapReadyCa
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+
+        switch (requestCode) {
+            case LOCATION_REQUEST_CODE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the location-related task you need to do.
+
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+                        Intent intent = new Intent(this, DirectionsActivity.class);
+                        startActivity(intent);
+                    }
+
+                } else {
+
+                    Toast.makeText(this, "Maps needs your location permission", Toast.LENGTH_SHORT).show();
+
+                }
+                return;
+            }
+
+        }
     }
 
 }
