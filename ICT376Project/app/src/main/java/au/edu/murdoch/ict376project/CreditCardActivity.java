b5ca5cf2224da8a57799d1ee333f1a9e91857773
EdwardPool <eddiepool@healthystridesfoundation.com>
2020-10-26 22:24:28 +0800

    Update to search fragment - radiobuttons now work to select platform and search from home fragment is still working but not filtered by platform. Users will need to select search via the drawer for the more powerful search option. Also commented code for creditcard activity is back to show we had code that was not linked to a 3rd party library.

--- ICT376Project/app/src/main/java/au/edu/murdoch/ict376project/CreditCardActivity.java
+++ ICT376Project/app/src/main/java/au/edu/murdoch/ict376project/CreditCardActivity.java
@@ -69,4 +69,83 @@ public class CreditCardActivity extends AppCompatActivity {
         onBackPressed();
         return true;
     }
+
+    // code that would have been used if not for the library that simplified the luhn authentication of credit cards
+
+    /*
+public class CreditCardActivity extends AppCompatActivity {
+    TextView amount;
+    Database db;
+    EditText payFname, payLname, payAddress, payEmail;
+    Button payNow;
+    String userFname, userLname, userAddress, userEmail;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credit_card);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        assert getSupportActionBar() != null;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        amount = findViewById(R.id.creditCardToPay);
+        payFname = findViewById(R.id.paymentFname);
+        payLname = findViewById(R.id.paymentLname);
+        payAddress = findViewById(R.id.paymentAddress);
+        payEmail = findViewById(R.id.paymentEmail);
+        Intent intent = getIntent();
+        int totalToPay = intent.getIntExtra("totalToPay", 0);
+        amount.setText(getString(R.string.please_pay, totalToPay));
+        // 1. get instance of shared preferences (prefs is the private pref file that stores the values put into in (below....)
+        SharedPreferences userDetails = getSharedPreferences("prefs", MODE_PRIVATE);
+        // 2. String name
+        String storedUserName;
+        db = new Database(this);
+        storedUserName = userDetails.getString("username", "");
+        assert storedUserName != null;
+        if(storedUserName.equals("")){
+            Toast.makeText(this, "Please enter details in all fields", Toast.LENGTH_SHORT).show();
+        }else{
+            Long userId = db.returnUserId(storedUserName);
+            ArrayList<String> dbArrayList = db.returnAllUserDetails(userId);
+            userFname = dbArrayList.get(0);
+            userLname = dbArrayList.get(1);
+            userAddress = dbArrayList.get(2);
+            userEmail = dbArrayList.get(4);
+        }
+        payFname.setText(userFname);
+        payLname.setText(userLname);
+        payAddress.setText(userAddress);
+        payEmail.setText(userEmail);
+        payNow = findViewById(R.id.payNow);
+        payNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(userEmail == null){
+                    // for anonymous purchases
+                    userEmail = payEmail.getText().toString();
+                    Toast.makeText(CreditCardActivity.this, "Thank you for ordering from ERE games, a confirmation email has been sent to: "+userEmail, Toast.LENGTH_LONG).show();
+                    db.clearCart();
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(CreditCardActivity.this, "Thank you for ordering from ERE games, a confirmation email has been sent to: "+userEmail, Toast.LENGTH_LONG).show();
+                    db.clearCart();
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+        db.close();
+    }
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        // replaced onBackPressed with return to main activity instead
+        */
+/*Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);*//*
+        return true;
+    }
+}*/
 }
