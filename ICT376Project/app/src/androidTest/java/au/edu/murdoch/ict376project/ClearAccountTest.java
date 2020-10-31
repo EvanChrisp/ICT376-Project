96dd21e799ee685ce0c3b28aaf947e045c145c43
Razecar <55474694+Razecar@users.noreply.github.com>
2020-10-31 16:29:38 +0800

    Add files via upload

---
+++ ICT376Project/app/src/androidTest/java/au/edu/murdoch/ict376project/ClearAccountTest.java
@@ -0,0 +1,49 @@
+package au.edu.murdoch.ict376project;
+
+import androidx.test.espresso.contrib.NavigationViewActions;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.contrib.DrawerActions.open;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ClearAccountTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule
+            = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void UpdateAccount() {
+        onView(withId(R.id.drawer_layout)).perform(open());
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_login));
+        onView(withId(R.id.loginButton)).check(matches(isDisplayed()));
+        onView(withId(R.id.usernameEt)).perform(typeText("Tester1"));
+        onView(withId(R.id.passwordEt)).perform(typeText("Test1!"), closeSoftKeyboard());;
+        onView(withId(R.id.loginButton)).perform(click());
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getInstrumentation().getContext());
+        onView(withText("My profile")).perform(click());
+        onView(withId(R.id.profileClearButton)).perform(click());
+        onView(withId(R.id.profileEditTextFname)).check(matches(withText("")));
+    }
+
+}
